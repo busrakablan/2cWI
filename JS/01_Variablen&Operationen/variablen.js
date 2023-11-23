89dfa1ce2d18b83d31685207d7e6e6c02b264a00
büsra <buesra.kablan@student.htldornbirn.at>
2023-11-23 09:31:17 +0100

    end of lesson

---
+++ JS/01_Variablen&Operationen/variablen.js
@@ -0,0 +1,18 @@
+// Variablen
+let a = 0;
+let b = 5;
+let c = 4;
+// Berechne folgendes und gib das Eregebnis über console.log aus
+// a + b + c
+ let result =  a + b + c; 
+    console.log(a + b + c);
+// (a + b) / c
+// c -a + b
+// a + b + c * 3
+//(a + b + c)/4 
+
+// Ändere die Variablen auf folgende Werte
+// a = 3
+// b = a + 3
+// c = c -3
+// gib die neuen Werte der Variablen aus

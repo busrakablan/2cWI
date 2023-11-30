592c3597bdd836931ed0297762b07889bf003b54
büsra <buesra.kablan@student.htldornbirn.at>
2023-11-30 08:24:57 +0100

    finished 01 JS Variablen&Operatoren

--- JS/01_Variablen&Operationen/variablen.js
+++ JS/01_Variablen&Operationen/variablen.js
@@ -4,15 +4,30 @@ let b = 5;
 let c = 4;
 // Berechne folgendes und gib das Eregebnis über console.log aus
 // a + b + c
- let result =  a + b + c; 
-    console.log(a + b + c);
+let result = a + b + c;
+console.log(result);
 // (a + b) / c
+let result2 = (a + b) / c;
+console.log(result2);
 // c -a + b
+let result3 = c - a + b;
+console.log(result3);
 // a + b + c * 3
-//(a + b + c)/4 
+let result4 = a + b + c * 3;
+console.log(result4);
+//(a + b + c)/4
+let result5 = (a + b + c) / 4;
+
 
 // Ändere die Variablen auf folgende Werte
 // a = 3
 // b = a + 3
 // c = c -3
 // gib die neuen Werte der Variablen aus
+
+a = 3;
+b = a + 3;
+c = c - 3;
+
+let result6 = a + b + c;
+console.log(result6);

e060a7f22b222d8ff65b7ef3adefe5542219e940
büsra <buesra.kablan@student.htldornbirn.at>
2023-11-30 09:28:14 +0100

    working on 03 js Aufgaben

---
+++ JS/03_Aufgabe zu Variablen/Uebung1.js
@@ -0,0 +1,18 @@
+ // Erstelle eine Variable banana mit dem Wert "Banane"
+ let banana = banana;
+// Erstelle eine Variable apple mit dem Wert "Apple"
+let apple = apple;
+// Erstelle eine Variable bananaPricePerKilo mit dem Wert 2.14
+let bananaPricePerKilo = 2.14;
+// Erstelle eine Variable applePricePerKilo mit dem Wert 3.43
+let applePricePerKilo = 3.43;
+// Ein Apfel hat durchschnittlich ein Gewicht von 0.34 Kilo
+let appledurchschnitt = 0.34;
+// Eine Banane hat durchschnittlich ein Gewicht von 0.22 Kilo
+let bananadurchschnitt = 0.22;
+// Gib folgendes aus: Anzahl Bananen/Äpfel pro Kilo, Preis pro Banane/Apfel
+// Preis von 8 Äpfeln
+console.log()
+// Preis von 17 Bananen
+// Preis von 1 Tonne Äpfel
+// Preis von 1 Tonne Bananen

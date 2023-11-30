264dcc2da5ed9ef1efc3fc07b7c0cefcc0cbac56
büsra <buesra.kablan@student.htldornbirn.at>
2023-11-30 09:08:01 +0100

    finisched with 02 JS strings&Zahlen

---
+++ JS/02_Strings&Zahlen/strings.js
@@ -0,0 +1,27 @@
+//1) Berechne das Ergebnis von 17.4 / 3.9. Gib folgenden Satz aus:
+
+let a = 17.4;
+let b = 3.9;
+
+let result = a - b;
+console.log(result);
+
+console.log("Das Ergebnis von " + a + " und " + b + " ist " + result);
+
+//The result of 17.4 divided by 3.9 is 4.461538461538462
+
+let result1 = a / b;
+console.log(result1);
+
+console.log("The result of " + a + " divided by " + b + " is " + result1);
+
+//2) Lege eine Variable firstname und eine Varialbe lastname an. Weise den Variablen Deinen Namen zu. Gib folgendes aus:
+
+let firstname = "Büsra";
+console.log(firstname);
+
+let lastname = "Kablan";
+console.log(lastname);
+
+//"Ich heiße VORNAME NACHNAME"
+console.log("Ich heiße " + firstname + " " + lastname);

3e2e72084bcc7d2004ad6c1e82327f100d29b232
büsra <buesra.kablan@student.htldornbirn.at>
2023-12-14 09:07:36 +0100

    Done with 04-Schleifen

--- JS/04_schleifen/schleifen.js
+++ JS/04_schleifen/schleifen.js
@@ -1,10 +1,37 @@
 // Zähle von 7 bis 45
-for (let count = 7; count < 45; count++); {
-console.log("Fertig"); }
-
+for (let count = 7; count <=45; count++) {
+  console.log(count);
+}
+console.log("Fertig");
 // Zähle von 9 bis 99
-// Zähle rückwärts von 10 bis 0
-// Zähle rückwärts von 1234 bis 1207
+for ( let count = 9; count <=99; count++) {
+    console.log(count);
+}
+console.log("Fertig");
 
+// Zähle rückwärts von 10 bis 0
+for ( let count = 10; count >=0; count--) {
+    console.log(count);
+}
+
+console.log("Fertig");
+
+// Zähle rückwärts von 1234 bis 1207
+for ( let count = 1234; count >=1207; count--) {
+    console.log(count);
+}
 // Gib Deinen Namen genau 8 mal aus
+for (let count = 0; count <8; count++) {
+    console.log( "Büsra");
+  }
+  console.log("Fertig");
+
 // Zähle von 0 auf 10, dann gib Deinen Namen aus, dann zähle von 10 auf 0
+for (let count = 0; count <=10; count++) {
+    console.log(count);
+  }
+ console.log("Büsra");
+  
+ for ( let count = 10; count >=0; count--) {
+    console.log(count);
+}

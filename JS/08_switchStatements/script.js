e9a9961dfbaee49433ddac59970b938227069999
büsra <buesra.kablan@student.htldornbirn.at>
2024-01-25 09:19:35 +0100

    done with o8_ switschstatements

--- JS/08_switchStatements/script.js
+++ JS/08_switchStatements/script.js
@@ -1,16 +1,18 @@
 // Erstelle eine Zufallszahl zwischen 5 und 10
-let randomNumber = Math.random() * 5 + 10;
-console.log(randomNumber);
-
-let random = Math.floor(randomNumber);
+let randomNumber = Math.random() * 6;
+let random = Math.floor(randomNumber) + 5;
 console.log(random);
-
 
 let a = random;
 // Wenn der Wert 10 ist gib aus Ten
-if (a=10) {
-    "Ten"
+if (a==10) {
+    console.log("Ten");
 }
 // Wenn der Wert 9 ist gib aus Nine
+if (a==9) {
+    console.log("Nine");
+}
 // Wenn der Wert 8 ist gib aus Eight
-// etc.
+if (a==8) {
+    console.log("Eight");
+}
